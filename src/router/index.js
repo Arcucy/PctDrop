5f46b5d45307aab6e79d3bcbfd869850e30c3046
Ayaka Neko <neko@ayaka.moe>
2020-10-09 19:07:04 +0800

    :package: Integrated with Vuex and Vue-Router
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Landing from '@/pages/Landing.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Landing',
+      component: Landing
+    }
+  ]
+})
